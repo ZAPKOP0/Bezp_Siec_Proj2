94c5ecd849588cee9e74fa7c80fdbf4034eb9f78
user <pawelkopiczak@gmail.com>
2026-05-25 15:30:37 +0200

    finalna wersja

--- src/Main.java
+++ src/Main.java
@@ -60,9 +60,6 @@ public class Main {
                         )
                 );
 
-                System.out.println(
-                        "Użytkownik został zapisany."
-                );
             }
 
         } catch (Exception e) {
